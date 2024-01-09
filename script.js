c8cc1f0623b97fc6d8e9c8755e4284a0b4051a57
Evan <j.evan.woods@gmail.com>
2024-01-09 12:44:22 -0500

    Hovering changes color and clear button.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('linked');
+const container = document.getElementById('gameContainer');
+
+function clearBoard() {
+  const tiles = document.querySelectorAll('.newColor');
+  tiles.forEach((tile) => {
+    tile.classList.remove('newColor');
+  });
+}
+
+const clearBtn = document.getElementById('clearBtn');
+clearBtn.addEventListener('click', clearBoard);
+
+function createBoard (numberOfTiles) {
+  for (let i = 0; i < numberOfTiles ** 2; i++) {
+    let tile = document.createElement('div');
+    tile.classList.remove
+    tile.classList.add('tile');
+    tile.addEventListener('mouseover', () => tile.classList.add('newColor'));
+    container.appendChild(tile);
+  }
+  console.log('ran');
+}
+
+createBoard(16);
