9a58bb7bdf3f84b09de9c823107bdfe440e64286
Evan <j.evan.woods@gmail.com>
2024-01-10 12:15:59 -0500

    Finished the project.

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 const container = document.getElementById('gameContainer');
+const slider = document.getElementById('slider');
+slider.addEventListener('input', labelSlider);
+
+const clearBtn = document.getElementById('clearBtn');
+clearBtn.addEventListener('click', clearBoard);
+
+const createBtn = document.getElementById('createBtn');
+createBtn.addEventListener('click', createButton);
+
+function createBoard (numberOfTiles) {
+  const height = 660 / numberOfTiles;
+  for (let i = 0; i < numberOfTiles ** 2; i++) {
+    let tile = document.createElement('div');
+    tile.classList.remove();
+    tile.classList.add('tile');
+    tile.style.height = height + 'px';
+    tile.style.width = height + 'px';
+    tile.addEventListener('mouseover', () => tile.classList.add('newColor'));
+    container.appendChild(tile);
+  }
+}
 
 function clearBoard() {
   const tiles = document.querySelectorAll('.newColor');
@@ -7,18 +28,24 @@ function clearBoard() {
   });
 }
 
-const clearBtn = document.getElementById('clearBtn');
-clearBtn.addEventListener('click', clearBoard);
+function removeTiles () {
+  const tiles = document.querySelectorAll('.tile')
+  tiles.forEach((tile) => {
+    tile.remove();
+  });
+}
 
-function createBoard (numberOfTiles) {
-  for (let i = 0; i < numberOfTiles ** 2; i++) {
-    let tile = document.createElement('div');
-    tile.classList.remove
-    tile.classList.add('tile');
-    tile.addEventListener('mouseover', () => tile.classList.add('newColor'));
-    container.appendChild(tile);
-  }
-  console.log('ran');
+function createButton () {
+  let numberOfTiles = slider.value;
+  removeTiles();
+  createBoard(numberOfTiles);
+  console.log('createButton ran');
+}
+
+function labelSlider() {
+  let value = document.getElementById('slider').value;
+  document.getElementById('sliderLabel').textContent = value;
+  console.log('label slider ran');
 }
 
 createBoard(16);
